83777303c5fdd812eb6bcf7630e05d72398bf855
Lucky <bitdaemons@gmail.com>
2022-06-19 22:42:25 +0100

    tru

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const web3 = new Web3(window.ethereum)
 
   document.getElementById("load_button").addEventListener("click", async () => {
-    const contract = new web3.eth.Contract(BitDaemonsTokenABI, "0xBEa7c3F2D91a9c6fD7F5aA9c803d4C31C1dB8db9")
+    const contract = new web3.eth.Contract(DaemonPunksTokenABI, "0x22DEa64A0e9ecbB13d2B0dD2d95a91A06DACb23B")
     const walletAddress = document.getElementById("wallet_address").value
     contract.defaultAccount = walletAddress
     const DaemonBalance = await contract.methods.balanceOf(walletAddress).call()
@@ -22,13 +22,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
       const tokenMetadata = await fetch(tokenMetadataURI).then((response) => response.json())
 
-      const spacePunkTokenElement = document.getElementById("nft_template").content.cloneNode(true)
-      spacePunkTokenElement.querySelector("h1").innerText = tokenMetadata["name"]
-      spacePunkTokenElement.querySelector("a").href = `https://opensea.io/assets/0x45db714f24f5a313569c41683047f1d49e78ba07/${tokenId}`
-      spacePunkTokenElement.querySelector("img").src = tokenMetadata["image"]
-      spacePunkTokenElement.querySelector("img").alt = tokenMetadata["description"]
+      const DaemonPunkTokenElement = document.getElementById("nft_template").content.cloneNode(true)
+      DaemonPunkTokenElement.querySelector("h1").innerText = tokenMetadata["name"]
+      DaemonPunkTokenElement.querySelector("a").href = `https://nftkey.app/collections/daemonpunks/token-details/?tokenId=${tokenId}`
+      DaemonPunkTokenElement.querySelector("img").src = tokenMetadata["image"]
+      DaemonPunkTokenElement.querySelector("img").alt = tokenMetadata["description"]
 
-      document.getElementById("nfts").append(spacePunkTokenElement)
+      document.getElementById("nfts").append(DaemonPunkTokenElement)
     }
   })
 })
