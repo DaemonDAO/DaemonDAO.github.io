e1c0d3ecff45e822bcfc7c89e7faa6f4443777c0
Lucky <bitdaemons@gmail.com>
2022-09-29 02:43:27 +0100

    brrr

--- js/walletviewer.js
+++ js/walletviewer.js
@@ -1185,7 +1185,20 @@ async function populateDOFOs(address) {
      bdgallery.innerHTML = galleryCode
   }
 }
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
 
 let timeRequested = 0;
 
