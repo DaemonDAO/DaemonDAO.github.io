0624061523a743302c1e2b0522e38cd53a414184
Lucky <bitdaemons@gmail.com>
2022-09-29 16:09:17 +0100

    donttryit

--- js/walletviewer.js
+++ js/walletviewer.js
@@ -1189,20 +1189,8 @@ async function populateDOFOs(address) {
      bdgallery.innerHTML = galleryCode
   }
 }
-var coll = document.getElementsByClassName("collapsible");
-var i;
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    }
-  });
-}
+
 
 let timeRequested = 0;
 
@@ -1223,3 +1211,18 @@ window.addEventListener('load', async () => {
   //document.querySelector("#btn-Donate").addEventListener("click", ramenIsOnTheMenu);
 
 });
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
