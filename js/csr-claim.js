75591c4120ffcd912099f921af4d49e06ba54a26
Lucky <bitdaemons@gmail.com>
2023-03-09 17:24:33 +0000

    hdhfd

--- js/csr-claim.js
+++ js/csr-claim.js
@@ -916,7 +916,6 @@ async function checkCSRBalance(){
   let turnContract = await new web3.eth.Contract(TurnABI, CA_turn);
   let balance = turnContract.methods.balances(tokenId);
   console.log(`There is ${balance} $CANTO to claim on Turnstile NFT #${tokenId}`);
-  return
 }
 
 async function claimCSR() {
