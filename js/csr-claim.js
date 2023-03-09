c96efa2a4bc88be592d74761adceddb2d13c6448
Lucky <bitdaemons@gmail.com>
2023-03-09 17:03:29 +0000

    WOW

--- js/csr-claim.js
+++ js/csr-claim.js
@@ -916,6 +916,8 @@ async function claimCSR() {
     let tokenId = $('#CSR_ID').val()
     const web3 = new Web3(provider);
     let turnContract = await new web3.eth.Contract(TurnABI, CA_turn);
+    let balance = turnContract.methods.balances(tokenId);
+    console.log(`There is ${balance} $CANTO to claim on Turnstile NFT #${tokenId}`);
     let withdraw = turnContract.methods.withdraw(tokenId, selectedAccount, 10000000);
     let gas = withdraw.estimateGas({from: selectedAccount});
 
