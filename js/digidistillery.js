a35b41ce257d0e690406fb5a35fbabc101a75dd2
Lucky <bitdaemons@gmail.com>
2023-04-07 04:48:46 +0100

    attributes

--- js/digidistillery.js
+++ js/digidistillery.js
@@ -501,8 +501,8 @@ async function populateNFTs(address) {
         <p><img alt="DIGIDMN_${tokenList[i]}" src=${tokenMetadata["image"]} /></p>
         <h3>DIGIDMN #${tokenList[i]}</h3>
         <h3>${tokenMetadata["name"]}</h3>
-        <p>FACTION: ${tokenMetadata["attributes"]} </br> 
-        PORTAL: ${tokenMetadata["attributes"]["DigiPortal"]}</p>
+        <p>FACTION: ${tokenMetadata.attributes["DigiFaction"]} </br> 
+        PORTAL: ${tokenMetadata.attributes}</p>
       </div>
       `;
      }
