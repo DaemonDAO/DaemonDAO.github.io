7f660071f495b751e19fde92692e1e87a36553be
Lucky <bitdaemons@gmail.com>
2023-06-09 16:02:50 +0100

    eureka?

--- js/digidistillery.js
+++ js/digidistillery.js
@@ -623,13 +623,14 @@ async function populateNFTs(address) {
   }
 
   let stakedList = await getMyStakedIds(DigiDistilleryCA, DigiDistilleryABI);
+  stakedList = Number(stakedList);
 
   document.getElementById("rye-staked-statement").innerHTML = `You have ${stakedList.length} staked DigiDaemons`
 
  let totalStaked = await getTotalStakedBalance();
  // let poolWeight = (100 * stakedList.length / totalStaked).toFixed(2);
  // console.log(`your Rye pool share is ${poolWeight}`);
- let poolWeight = (BigInt(100) * BigInt(stakedList.length) / totalStaked);
+ let poolWeight = (100 * stakedList.length / Number(totalStaked));
  poolWeight = Number(poolWeight).toFixed(2); // Convert back to number for simplicity and practicality
  console.log(`your Rye pool share is ${poolWeight}`);
   document.getElementById("rye-coin-balance").innerHTML = `<p>Your pool share: ${poolWeight}%</p>`;
