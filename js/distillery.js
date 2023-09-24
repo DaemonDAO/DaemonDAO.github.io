3626526c3c10867250c1ce329c9de7e87e3fa70b
Lucky <bitdaemons@gmail.com>
2023-09-24 21:58:10 +0100

    pppfffff

--- js/distillery.js
+++ js/distillery.js
@@ -1,10 +1,10 @@
-import { RainbowKit } from '@rainbow-me/rainbowkit';
-import DigiDistilleryABI from "../abi/digidistillery_contract.js";
-import DigiTokenABI from "../abi/digidaemons_token.js";
+//import { RainbowKit } from '@rainbow-me/rainbowkit';
+//import DigiDistilleryABI from "../abi/digidistillery_contract.js";
+//import DigiTokenABI from "../abi/digidaemons_token.js";
 
-"use strict";
-const DigiDistilleryCA = "0x3B21c992D69Dde32aee0c935A2806743f9EE8C2d";
-const DigiTokenCA = "0x6eA48824253f64662945Ae77A790331D7183f8c0";
+//"use strict";
+//const DigiDistilleryCA = "0x3B21c992D69Dde32aee0c935A2806743f9EE8C2d";
+//const DigiTokenCA = "0x6eA48824253f64662945Ae77A790331D7183f8c0";
 
 
 const rainbowKit = new RainbowKit();
